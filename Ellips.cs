b65f2aae9075f5ab330b199f2bbdddb985f1399d
user <user@WIN-0PO8TPIL1T4>
2022-02-16 20:04:17 +0300

    Добавьте файлы проекта.

---
+++ Ellips.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    public class Ellips
+    {
+        public int A { get; set; }
+        public int B { get; set; } 
+        
+        public Ellips (int a, int b)
+        {
+            if (a <= 0)
+            {
+                throw new ArgumentException("The radius is less than or equal to zero");
+            }
+            if (b <= 0)
+            {
+                throw new ArgumentException("The radius is less than or equal to zero");
+            }
+
+            A = a;
+            B = b;
+        }
+        public double GetSquare()
+        {
+            return Math.Round(Math.PI * A * B, 2);
+        }
+        public double GetLength()
+        {
+            return Math.Round(Math.PI * (A + B), 2); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using HomeWork1;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Ellips ellipse = new (15, 17);
+            Console.WriteLine($"Длина эллипса: {ellipse.GetLength()}");
+            Console.WriteLine($"Площадь эллипса: {ellipse.GetSquare()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
