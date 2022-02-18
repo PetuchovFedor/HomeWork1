a6cb6eb314bb0c12c1dd5d66c29521cdb45ab9ba
PetuchovFedor <71184846+PetuchovFedor@users.noreply.github.com>
2022-02-18 14:29:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Point centerpoint = new Point
+        Point centerPoint = new Point
         {
             X = 10,
             Y = 10
         };
         try
         {
-            Ellips ellipse = new (centerpoint, 15, 17);
+            Ellips ellipse = new (centerPoint, 15, 17);
             Console.WriteLine($"Длина эллипса: {ellipse.GetLength()}");
             Console.WriteLine($"Площадь эллипса: {ellipse.GetSquare()}");
         }
